6f2c4a7350ee869f425510f1d086ae472804a150
Peer Naupold <greenion@gmx.de>
2019-11-10 23:53:59 +0100

    Added the functionality to write to and read from a list and display the message to the user

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    """ Add messages to the 'messages' list """
+    messages.append("{}:{}".format(username, message))
+
+def get_all_messages():
+    """ Get all of the messages and separate them with a 'br' """
+    return "<br>".join(messages)
+
 
 @app.route('/')
 def index():
-    return "To send a message use /<USERNAME>/<MESSAGE>"
+    """ Main page with instructions """
+    return "To send a message use /USERNAME/MESSAGE"
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """ Display chat messages """
+
+    return "<h1>Welcome, {0}</h1>{1} ".format(username, get_all_messages())
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """ Create a new message and redirect back to the chat page """
+    add_messages(username, message)
+    return redirect ("/" + username)
+
 
 
 app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 5000)), debug=True)
